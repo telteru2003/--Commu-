36063b47c93e16c0aa60ee7e75be762ce8ceca8b
YutaMorimoto <135404018+telteru2003@users.noreply.github.com>
2023-11-22 08:08:19 +0000

    [11/22] 管理者機能START

--- app/controllers/public/families_controller.rb
+++ app/controllers/public/families_controller.rb
@@ -1,13 +1,14 @@
 class Public::FamiliesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_family, only: [:show, :edit, :update, :permit, :destroy]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy, :memberships]
+  before_action :set_family, only: [:show, :edit, :update, :memberships, :permit, :destroy]
+  before_action :set_user
 
   def new
     @family = Family.new
   end
 
   def create
-    @user = current_user
     @family = Family.new(family_params)
     @family.owner_id = current_user.id
 
@@ -43,6 +44,14 @@ class Public::FamiliesController < ApplicationController
     end
   end
 
+  def memberships
+    @memberships = @family.memberships.page(params[:page])
+  end
+
+  def permit
+    @memberships = @family.memberships.page(params[:page])
+  end
+
   def destroy
     @family.destroy!
     flash[:notice] = 'グループを削除しました'
@@ -55,8 +64,20 @@ class Public::FamiliesController < ApplicationController
     @family = Family.find(params[:id])
   end
 
-  def family_params
-    params.require(:family).permit(:name, :owner_id)
+  def set_user
+    @user = current_user
   end
 
+  def family_params
+    params.require(:family).permit(:name, places_attributes: [:id, :name, :_destroy])
+  end
+
+  # params[:id]を持つ@familyのowner_idカラムのデータと自分のユーザーIDが一緒かどうかを確かめる。
+  # 違う場合、処理をする。グループ一覧ページへ遷移させる。before_actionで使用する。
+  def ensure_correct_user
+    @family = Family.find(params[:id])
+    unless @family.owner_id == current_user.id
+      redirect_to show_user_path(current_user.id), alert: "グループオーナーのみ編集が可能です"
+    end
+  end
 end
