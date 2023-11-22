d440824b606f986cb6b26c4894e1e559da016050
YutaMorimoto <135404018+telteru2003@users.noreply.github.com>
2023-11-22 13:13:54 +0000

    [11/22] 管理者機能FINISH

--- app/controllers/public/family_users_controller.rb
+++ app/controllers/public/family_users_controller.rb
@@ -4,37 +4,20 @@ class Public::FamilyUsersController < ApplicationController
   def create
     @family_user = FamilyUser.create(family_id: family_user_params[:family_id], user_id: family_user_params[:user_id])
     Membership.find(family_user_params[:membership_id]).destroy!
-    redirect_to show_user_path(current_user), notice:"「#{@family_user.user.name}」が、コミュニティ：#{@family_user.family.name}へ加入しました。"
-
-    # @family = Family.find(params[:family_id])
-    # @membership = Membership.find(params[:membership_id])
-    # @family_user = FamilyUser.create(user_id: @membership.user_id, family_id: params[:family_id])
-    # @membership.destroy #参加希望者リストから削除する
-    # redirect_to request.referer
+    redirect_to show_user_path(current_user), notice:"「#{@family_user.user.name}」が#{@family_user.family.name}へ加入しました"
   end
 
   def destroy
     @family_user = FamilyUser.find_by(family_id: params[:family_id], user_id: params[:user_id])
-    @family_user.destroy!
-    @comminity = Family.find(params[:family_id])
-    redirect_to show_user_path(current_user), notice: "グループ「#{@comminity.name}」を退会しました。"
 
-    # @family = Family.find(params[:family_id])
-    # @membership = Membership.find(params[:membership_id])
-    # @family_user = FamilyUser.find_by(user_id: @membership.user_id, family_id: params[:family_id])
-
-    # if @family_user
-    #   @family_user.destroy
-    # end
-
-    # redirect_to request.referer
-
-  end
-
-  private
-
-  def family_user_params
-    params.permit(:family_id, :user_id, :membership_id)
+    if @family_user
+      @family_user.destroy!
+      @comminity = Family.find(params[:family_id])
+      redirect_to show_user_path(current_user), notice: "「#{@comminity.name}」を退会しました"
+    else
+      # エラー処理などを行うか、リダイレクトなどを行って適切な対応をする
+      redirect_to show_user_path(current_user), alert: "エラーが発生しました"
+    end
   end
 
 end
